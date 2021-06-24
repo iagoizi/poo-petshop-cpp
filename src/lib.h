08c4a0f6c5c3823ef7876b6f8e43f341e41fcd5c
Iago Izidorio Lacerda <iagoilacerda@gmail.com>
2021-06-24 19:07:03 -0300

    .h para macros globais e bibliotecas que todos vamos usar

---
+++ src/lib.h
@@ -0,0 +1,18 @@
+#ifndef LIB_H
+#define LIB_H
+
+#define M 51
+#define DEFAULT_PATH string("../dados/")
+#define CONTAS_PETSHOP DEFAULT_PATH + string("contas.dat")
+
+#include <iostream>
+#include <string>
+#include <string.h>
+#include <stdlib.h>
+#include <algorithm>
+
+#include <fstream>
+#include <vector>
+using namespace std;
+
+#endif
