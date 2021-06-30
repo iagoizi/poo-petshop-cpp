55e92c3bb24b0e38cfa95d535ee0ccb7eda81e96
Iago Izidório Lacerda <iagoilacerda@gmail.com>
2021-06-29 23:25:25 -0300

    Adicionando caminhos para views

---
+++ src/main.hpp
@@ -0,0 +1,13 @@
+#ifndef MAIN_HPP
+#define MAIN_HPP
+#include "lib/model/hpp/lib.hpp"
+#include "lib/model/hpp/petshop.hpp"
+#include "lib/view/hpp/menu.hpp"
+
+//Usado apenas nos testes
+
+#include "lib/model/hpp/vendedor.hpp"
+#include "lib/model/hpp/veterinario.hpp"
+#include "lib/model/hpp/administrador.hpp"
+
+#endif
