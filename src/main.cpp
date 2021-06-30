01206531c2e3802a5b33bb703edfc6f92ae880a4
Iago Izidório Lacerda <iagoilacerda@gmail.com>
2021-06-29 23:27:04 -0300

    Testes iniciais Menus >FUNCIONANDO<

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,24 @@
-#include "lib/model/hpp/lib.hpp"
-
+#include "main.hpp"
 int main()
 {
-    cout << "Não implementado ainda";
+    /*cadastro manual de usuário <APENAS PARA TESTES>*/
+    PetShop petshop = PetShop("Puppycat");
+    petshop.getUsuarios().push_back(Vendedor(&petshop, "Vendy Claudia", "vendedor", "senha"));
+    petshop.getUsuarios().push_back(Veterinario(&petshop, "Vet Samira", "veterniria", "senha"));
+    petshop.getUsuarios().push_back(Administrador(&petshop, "Adimênia"));
+    string usuarioTeste = "vendedor";
+    string senhaTeste = "senha";
+
+    //Aqui começa o main de verdade
+    int op = 1;
+    bool loggedIn = false;
+    Menu *menu = petshop.login(usuarioTeste, senhaTeste, &loggedIn);
+    cout << (loggedIn ? "Login efetuado com sucesso" : "Usuário ou senha incorretos") << endl;
+    do
+    {
+        menu->printMenu();
+        cin >> op;
+        menu->realizaOperacao(op);
+    } while (op != OPCODE_SAIDA);
     return 0;
 }
