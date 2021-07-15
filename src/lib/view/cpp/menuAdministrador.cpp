9bf15c9b594696ac1fb4e2f7005036d70f2df0e0
Danilo.Cesar <danilossoares1999@gmail.com>
2021-07-15 20:51:09 -0300

    Comentarios do Menu Administrador

--- src/lib/view/cpp/menuAdministrador.cpp
+++ src/lib/view/cpp/menuAdministrador.cpp
@@ -1,14 +1,18 @@
 #include "../hpp/menuAdministrador.hpp"
 #include "../../model/hpp/administrador.hpp"
 
+//Construtor do menu administrador
 MenuAdministrador::MenuAdministrador(PetShop *petshop) : Menu(petshop), MenuVendedor(petshop), MenuVeterinario(petshop)
 {
 }
 
+//Destrutor do menu administrador
 MenuAdministrador::~MenuAdministrador()
 {
 }
 
+/*Transforma o usuario logado em um usuario do tipo administrador,
+permitindo a esse usuario o acesso a todas as funções do administrador*/
 Administrador *MenuAdministrador::getAdministrador()
 {
     Administrador *administrador = dynamic_cast<Administrador *>(this->petshop->getSessaoAtual());
@@ -20,6 +24,7 @@ Administrador *MenuAdministrador::getAdministrador()
     return administrador;
 }
 
+//Menu responsavel pelo cadastro dos vendedores
 void MenuAdministrador::menuCadastrarVendedor()
 {
     printTitulo("Cadastrando Vendedor...");
@@ -29,6 +34,7 @@ void MenuAdministrador::menuCadastrarVendedor()
     string senha;
     double salario;
 
+    //Saida e entrada de dados referentes ao vendedor
     cout << "Vendedor" << endl
          << "\tNome: ";
     cin.ignore();
@@ -40,12 +46,14 @@ void MenuAdministrador::menuCadastrarVendedor()
     cout << "\tSalário: ";
     cin >> salario;
 
+    //Chamada da função que cadastra o vendedor. Esta sendo passado todas as informações para cadastro
     administrador->cadastrarVendedor(nome, usuario, senha, salario);
     this->popUp = "Cadastro realizado com sucesso!";
 }
 
+//Menu que cadastra os veterinarios do PetShop
 void MenuAdministrador::menuCadastrarVeterinario()
-{
+{   
     printTitulo("Cadastrando Veterinário...");
     Administrador *administrador = getAdministrador();
     string nome;
@@ -53,6 +61,7 @@ void MenuAdministrador::menuCadastrarVeterinario()
     string senha;
     double salario;
 
+    //Entrada e Saida de dados para coletar informações e realizar o cadastro do Veterinario
     cout << "Veterinário" << endl
          << "\tNome: ";
     cin.ignore();
@@ -63,10 +72,12 @@ void MenuAdministrador::menuCadastrarVeterinario()
     cin >> senha;
     cout << "\tSalário: ";
     cin >> salario;
+    //Função na qual passamos todos os dados para cadastrar um novo usuario veterinario
     administrador->cadastrarVeterinario(nome, usuario, senha, salario);
     this->popUp = "Cadastro realizado com sucesso!";
 }
 
+/* Menu semelhante aos anteriores porem direcionado ao cadastro de produtos */
 void MenuAdministrador::menuCadastrarProdutos()
 {
     printTitulo("Cadastrando Produto...");
@@ -76,6 +87,7 @@ void MenuAdministrador::menuCadastrarProdutos()
     int quantidade;
     long id;
 
+    /* Entrada de dados para realizar o cadastro de novos produtos */
     cout << "Produto" << endl
          << "\tNome: ";
     cin.ignore();
@@ -88,18 +100,25 @@ void MenuAdministrador::menuCadastrarProdutos()
     cin >> id;
 
     bool jaCadastrado = false;
+    /* Função que realiza a busca de produto, retornando true/false. 
+    Informação utilizada para não cadastrar 2 produtos semelhantes */
     administrador->buscarProduto(id, &jaCadastrado);
     if (jaCadastrado)
-    {
+    {   
+        /*Print caso o produto ja esteja cadastrado*/  
         this->popUp = "Produto já cadastrado";
     }
     else
     {
+        /*Caso o produto ainda não tenha sido cadastrado realizaremos seu cadastro.
+        Sera passado todos os dados para tal ação. */
         administrador->cadastrarProduto(nome, preco, quantidade, id);
+        //cout que confirma o cadastramento
         this->popUp = "Cadastro realizado com sucesso!";
     }
 }
 
+/* Mostra o menu para cadastrar novos serviços*/
 void MenuAdministrador::menuCadastrarServicos()
 {
     printTitulo("Cadastrando Serviço...");
@@ -108,6 +127,8 @@ void MenuAdministrador::menuCadastrarServicos()
     double preco;
     long id;
 
+    //Solicita os dados necessarios para o cadastro do serviço.
+    //Permite a entrada de dados para o cadastro
     cout << "Serviço" << endl
          << "\tNome: ";
     cin.ignore();
@@ -118,48 +139,67 @@ void MenuAdministrador::menuCadastrarServicos()
     cin >> id;
 
     bool jaCadastrado = false;
+    //Confere se ja possui um servico cadastrado no ID digitado
     administrador->buscarServico(id, &jaCadastrado);
     if (jaCadastrado)
     {
+        //Caso ja possua um servico cadastrado nesse id
         this->popUp = "Serviço já cadastrado";
     }
     else
     {
+        //Função que realiza o cadastro do serviço 
         administrador->cadastrarServico(nome, preco, id);
         this->popUp = "Cadastro realizado com sucesso!";
     }
 }
 
+//Lista todos os funcionarios cadastrados
 void MenuAdministrador::menuListarFuncionarios()
 {
     printTitulo("Listando Funcionários...");
+    //Pega os dados do administrador do sistema
     Administrador *administrador = getAdministrador();
+    //Chamada da função que realiza a listagem dos usuarios no terminal
     administrador->listarFuncionarios();
+    //Função para usuario solicitar o continuação da execução do programa
     esperarEnter();
 }
 
+//Menu para a reposição do estoque dos produtos
 void MenuAdministrador::menuReporEstoque()
 {
     printTitulo("Repondo Estoque...");
+    
+    //Permite ao usuario acesso a função do administrador
     Administrador *administrador = getAdministrador();
+    
+    //Função que lista os produtos cadastrados     
     administrador->listarProdutos();
+
     int quantidade;
     long id;
     bool jaCadastrado = false;
     double preco;
+
+    //Entrada dos dados para repor o estoque
     cout << "Produto" << endl;
     cout << "\tID: ";
     cin >> id;
     cout << "\tPreço de aquisição: ";
     cin >> preco;
 
+    //Confere se o produto esta cadastrado
     Produto produto = administrador->buscarProduto(id, &jaCadastrado);
     string nomeProduto = produto.getNome();
 
     if (jaCadastrado)
-    {
+    {   
+        //Solicita a quantidade de itens que iremos repor do produto solicitado
         cout << "\tQuantidade: ";
         cin >> quantidade;
+        /*Função que ira realizar todos os procedimentos de reposição de estoque: aumento da quantidade disponivel,
+        registro da compra*/
         administrador->reposicaoEstoque(produto, quantidade, nomeProduto, (preco * quantidade));
         this->popUp = "Reposição de estoque realizada com sucesso!";
     }
@@ -169,13 +209,16 @@ void MenuAdministrador::menuReporEstoque()
     }
 }
 
+//Menu para pagar as contas 
 void MenuAdministrador::menuPagamentoContas()
 {
     printTitulo("Pagando Contas...");
+    //Permite ao usuario acesso a função do administrador
     Administrador *administrador = getAdministrador();
     string descricao;
     double valor;
 
+    //Entrada de todos os dados para realizar o pagamento de contas
     cout << "Conta" << endl
          << "\tDescricao: ";
     cin.ignore();
@@ -194,18 +237,23 @@ void MenuAdministrador::menuPagamentoContas()
     cout << "\tValor: ";
     cin >> valor;
 
+    //Função que realiza o cadastro da conta paga
     administrador->pagarConta(descricao, data, valor);
     this->popUp = "Pagamento realizado com sucesso!";
 }
 
+//Menu mostrado qual escolhemos gerar relatorios
 void MenuAdministrador::menuRelatorios()
 {
     printTitulo("Relatórios");
     Administrador *administrador = getAdministrador();
+    //Chamada da função que gera relatorio
     administrador->gerarRelatorio();
+    //Função que permite continuar a execução do programa
     esperarEnter();
 }
 
+//Função que mostra todas as opções do menu administrador
 void MenuAdministrador::printMenu()
 {
     Menu::printMenu();
@@ -229,54 +277,58 @@ void MenuAdministrador::printMenu()
          << endl;
 }
 
+/*Função responsável pelo gerenciamento das decisoes do usuario,
+mostrando os menus de acordo com a escolha do administrador*/
 void MenuAdministrador::realizaOperacao(int op)
 {
     Menu::realizaOperacao(op);
+    //Espaço para organizar o menu
     cout << endl
          << endl;
+    //Switch case que organiza as escolhas dos menus
     switch (op)
     {
-    case VENDER_PRODUTO_ADMIN:
+    case VENDER_PRODUTO_ADMIN: // 6
         menuVenderProduto();
         break;
-    case VENDER_SERVICO_ADMIN:
+    case VENDER_SERVICO_ADMIN: // 7
         menuVenderServico();
         break;
-    case CADASTRAR_CLIENTE_ADMIN:
+    case CADASTRAR_CLIENTE_ADMIN: // 1
         menuCadastrarCliente();
         break;
-    case LISTAR_CLIENTES_ADMIN:
+    case LISTAR_CLIENTES_ADMIN: // 9
         menuListarClientes();
         break;
-    case CADASTRAR_PRODUTOS_ADMIN:
+    case CADASTRAR_PRODUTOS_ADMIN: // 4
         menuCadastrarProdutos();
         break;
-    case CADASTRAR_SERVICOS_ADMIN:
+    case CADASTRAR_SERVICOS_ADMIN: // 5
         menuCadastrarServicos();
         break;
-    case REGISTRAR_TRATAMENTO_ADMIN:
+    case REGISTRAR_TRATAMENTO_ADMIN: // 11
         menuRegistrarTratamento();
         break;
-    case LISTAR_ORDENS_SERVICO_ADMIN:
+    case LISTAR_ORDENS_SERVICO_ADMIN: // 10
         menuListarOrdensDeServicos();
         break;
-    case CADASTRAR_VENDEDOR:
+    case CADASTRAR_VENDEDOR: // 2
         menuCadastrarVendedor();
         break;
-    case CADASTRAR_VETERINARIO:
+    case CADASTRAR_VETERINARIO:  // 3
         menuCadastrarVeterinario();
         break;
 
-    case LISTAR_FUNCIONARIOS:
+    case LISTAR_FUNCIONARIOS:  // 8
         menuListarFuncionarios();
         break;
-    case REPOR_ESTOQUE:
+    case REPOR_ESTOQUE: // 12
         menuReporEstoque();
         break;
-    case PAGAR_CONTAS:
+    case PAGAR_CONTAS: // 13
         menuPagamentoContas();
         break;
-    case GERAR_RELATORIOS:
+    case GERAR_RELATORIOS: // 14
         menuRelatorios();
         break;
     default:
--- src/lib/view/hpp/menuAdministrador.hpp
+++ src/lib/view/hpp/menuAdministrador.hpp
@@ -9,6 +9,7 @@
 #include "menuVeterinario.hpp"
 #include "menuVendedor.hpp"
 
+/*Macros que gerenciam as funcionalidades do menu administrador*/
 #define CADASTRAR_CLIENTE_ADMIN 1
 #define CADASTRAR_VENDEDOR 2
 #define CADASTRAR_VETERINARIO 3
@@ -26,23 +27,37 @@
 
 using namespace std;
 class Administrador;
+//Menu administrador herda funções do menu Vendedor e Veterinario
 class MenuAdministrador : public MenuVendedor, public MenuVeterinario
 {
     //private:
+    //Tem o intuito de retornar os dados do administrador logado
     Administrador *getAdministrador();
+    //Função resposavel pelo menu de cadastro de um novo vendedor
     void menuCadastrarVendedor();
+    //Função resposavel pelo menu de cadastro de um novo veterinario
     void menuCadastrarVeterinario();
+    //Função do menu para o cadastro de um novo produto
     void menuCadastrarProdutos();
+    //Função do menu para o cadastro de um novo serviço
     void menuCadastrarServicos();
+    //Menu da opção que lista todos os funionarios cadastrados no PetShop
     void menuListarFuncionarios();
+    //Menu mostrado para a opção de repor o estoque
     void menuReporEstoque();
+    //Menu que gerencia o pagamento das contas
     void menuPagamentoContas();
+    //Menu que gera os relatorios
     void menuRelatorios();
 
 public:
+    //Construtor do menu administrador
     MenuAdministrador(PetShop *petshop);
+    //Destrutor do menu administrador
     ~MenuAdministrador();
+    //Função que mostra o menu
     virtual void printMenu();
+    //Função que gerencia as decisões do usuario
     virtual void realizaOperacao(int op);
 };
 #endif
